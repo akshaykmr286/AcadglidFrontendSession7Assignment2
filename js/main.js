b58e737c8dd39e6950bf807cd260db56c72fcbdc
Akshay Kumar Nayak <akshaykmr286@gmail.com>
2017-06-27 13:46:37 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 /*
 this is the function called on click of the input box
 */
+var age_helper = "Please,Enter Your AGE";
+var name_helper = "Enter Your NAME";
+var email_helper = "Your EMAIL Address";
 function help(id){
 	//document.getElementById("helptext").innerHTML=id;
     if(id == "name"){
-    	document.getElementById("helptext").innerHTML="Enter Your NAME";
+    	document.getElementById("helptext").innerHTML= name_helper;
+        //name_helper variable is passed a closure
     }
     if(id == "email"){
-    	document.getElementById("helptext").innerHTML="Your EMAIL Address";
+    	document.getElementById("helptext").innerHTML= email_helper;
+        //email_helper variable is passed as closure
     }
     if(id == "age"){
-    	document.getElementById("helptext").innerHTML="Please,Enter Your AGE";
+        document.getElementById("helptext").innerHTML= age_helper;
+        //age_helper variable is passed as closure here
     }
 }
 
